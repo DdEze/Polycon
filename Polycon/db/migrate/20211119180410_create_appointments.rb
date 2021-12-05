d3d9e21b1c835401087adfd567e191e9b45e9ea1
Ezequiel <ezequielensenada@yahoo.com.ar>
2021-12-05 10:50:18 -0300

    update db

--- Polycon/db/migrate/20211119180410_create_appointments.rb
+++ Polycon/db/migrate/20211119180410_create_appointments.rb
@@ -1,11 +1,11 @@
 class CreateAppointments < ActiveRecord::Migration[6.1]
   def change
     create_table :appointments do |t|
-      t.datetime :date
-      t.string :name
-      t.string :surname
-      t.integer :phone
-      t.string :notes
+      t.datetime :date, null: false
+      t.string :name, null: false
+      t.string :surname, null: false
+      t.integer :phone, null: false
+      t.string :notes, null: false, default: "No hay notas"
       t.belongs_to :professional, null: false, foreign_key: true
 
       t.timestamps
